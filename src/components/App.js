cc8c43b0c70600c8b02d24f2aab02759069066e9
navib <ivan.baena.dev@gmail.com>
2019-10-25 17:14:53 -0400

    init

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  onSearchSubmit(query) {
+    console.log("app", query);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
